816dcb74eba51efe2236a725c04a4a1f668e4b79
asyylz <49663592+asyylz@users.noreply.github.com>
2024-02-24 00:46:39 +0000

    update to public folder

--- src/components/Horoscope.jsx
+++ src/components/Horoscope.jsx
@@ -1,26 +1,25 @@
 export default function Horoscope({ dataArray }) {
-    console.log(dataArray);
-    return (
-      <div className="row">
-        {dataArray.map(({ id, title, desc, image, date }) => (
-          <div key={id} className="col-xl-4 col-md-6  col-xs-12">
-            <div className="card">
-              <div className="cover item-a">
-                <h3>{date}</h3>
-                <h1>{title}</h1>
-                <img
-                  style={{ width: "100%", height: "100%" }}
-                  src={image}
-                  alt=""
-                />
-                <div className="card-back">
-                  <p>{desc}</p>
-                </div>
+  console.log(dataArray);
+  return (
+    <div className="row">
+      {dataArray.map(({ id, title, desc, image, date }) => (
+        <div key={id} className="col-xl-4 col-md-6  col-xs-12">
+          <div className="card">
+            <div className="cover item-a">
+              <h3>{date}</h3>
+              <h1>{title}</h1>
+              <img
+                src={image}
+                style={{ width: "100%", height: "100%" }}
+                alt=""
+              />
+              <div className="card-back">
+                <p>{desc}</p>
               </div>
             </div>
           </div>
-        ))}
-      </div>
-    );
-  }
-  
+        </div>
+      ))}
+    </div>
+  );
+}
